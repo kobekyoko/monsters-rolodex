722388d1b3370152830319e7203099a10ae6ba3d
Kyoko Onoda <kobepikomaru@gmail.com>
2020-01-29 18:04:27 +0900

    Updates

---
+++ precache-manifest.fd38df635fda6f1cf36210e26dd4195a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2e46af9cb444c8457a48ea9649f6f8d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d53b3d2e8cb025a78cc9",
+    "url": "/monsters-rolodex/static/css/main.91ffffbc.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d53b3d2e8cb025a78cc9",
+    "url": "/monsters-rolodex/static/js/main.22e51446.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
